989f03fd87ecc0f80ca82393a2cf49d0a1d1a408
anusha maram <maramanu925@gmail.com>
2026-04-20 21:56:04 +0530

    admin model commiting

--- models/admin.model.js
+++ models/admin.model.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema(
   {
+    name: { type: String, trim: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    mobile: { type: String, trim: true },
+    address: { type: String, trim: true },
     isDeleted: { type: Boolean, default: false }
   },
   { timestamps: true }
